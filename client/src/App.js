38cfff429181968cdee761e49eab4f395779eff2
jimmykiang <castlevanity@gmail.com>
2022-06-05 22:57:31 +0200

    docker-rt title

--- client/src/App.js
+++ client/src/App.js
@@ -11,16 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <p>Docker-RT</p>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
